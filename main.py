3efbdafcadb71fd35ca0ef7451e9872c9867d9a8
BaboJunYoung <junyoung090115@gmail.com>
2026-06-19 16:23:43 +0900

    화면 업데이트

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import pygame
 SCREEN_SIZE = (800, 600)
 
 pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE)
+screen = pygame.display.set_mode(SCREEN_SIZE, pygame.RESIZABLE)
 pygame.display.set_caption("소환사 주문가게에 오신 여러분, 환영합니다.")
 clock = pygame.time.Clock()
 
 
-# background = pygame.image.load("./assets/images/game_play_bg.png")
-background = pygame.image.load("./assets/images/game_intro_bg.png")
+background_original = pygame.image.load("./assets/images/game_play_bg.png")
+background = pygame.transform.smoothscale(background_original, SCREEN_SIZE)
+# background = pygame.image.load("./assets/images/game_intro_bg.png")
 
 running = True
 
@@ -20,9 +21,25 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.VIDEORESIZE:
+            new_width = event.w
+            new_height = event.h
+
+            # screen = pygame.display.set_mode((new_width, new_height), pygame.RESIZABLE)
+
+            background = pygame.transform.scale(background_original, (new_width, new_height))
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             # 좌: 1
             # 휠: 2
             # 우: 3
             if event.button == 1: # 좌클릭
                 print("hello world!")
+        
+
+    # 화면 업데이트
+    screen.blit(background, (0, 0)) # 배경
+    
+    pygame.display.flip() # update() 랑 똑같은 기능, update(Rect)로 Rect 범위만 업데이트 가능
+
+pygame.quit()
